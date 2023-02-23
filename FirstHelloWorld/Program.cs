8a813ddc1f4dc10424bc0bdaaab8110e5f75ebf2
Giorgi Tskhvediani <121679453+avoedigital@users.noreply.github.com>
2023-02-23 20:47:53 +0400

    Update Program.cs

--- FirstHelloWorld/Program.cs
+++ FirstHelloWorld/Program.cs
@@ -40,21 +40,21 @@ class HomeWork
     }
     // 2. შევქმნათ მეთოდი. ვთხოვოთ მომხმარებელს შეიყვანოს ოთხი ნებისმიეირ ტიპის რიცხვი (ანუ არა მხოლოდ ინტეჯერი). მეთოდის ფარგლებში უნდა მოიძებნოს ამ ოთხი რიცხვიდან ყველაზე დიდი და ყველაზე პატარა.
     // მეთოდმა უნდა გამოიყვანოს კონსოლში ეს ორი რიცხვი.
-    static void MinMax(double x1, double y1, double z, double w)
+static void MinMax(double x1, double y1, double z, double w)
     {
-        if (x1 > y1 && x1 > z && x1 > w)
+        if (x1 >= y1 && x1 >= z && x1 >= w)
         {
             Console.WriteLine("The Highest number: " + x1);
         }
-        else if (y1 > z && y1 > w && y1 > x1)
+        else if (y1 >= z && y1 >= w && y1 >= x1)
         {
             Console.WriteLine("The Highest number: " + y1);
         }
-        else if (z > w && z > x1 && z > y1)
+        else if (z >= w && z >= x1 && z >= y1)
         {
             Console.WriteLine("The Highest number: " + z);
         }
-        else if (w > x1 && w > z && w > y1)
+        else if (w >= x1 && w >= z && w >= y1)
         {
             Console.WriteLine("The Highest number: " + w);
         } else
@@ -62,26 +62,23 @@ class HomeWork
             Console.WriteLine("Numbers is equal");
         }
 
-        if (x1 < y1 && x1 < z && x1 < w)
+        if (x1 <= y1 && x1 <= z && x1 <= w)
         {
             Console.WriteLine("The Lowest number is " + x1);
         }
-        else if (y1 < x1 && y1 < z && y1 < w)
+        else if (y1 <= x1 && y1 <= z && y1 <= w)
         {
             Console.WriteLine("The lowest number is " + y1);
         }
-        else if (z < x1 && z < y1 && z < w)
+        else if (z <= x1 && z <= y1 && z <= w)
         {
             Console.WriteLine("The lowest number is " + z);
 
         }
-        else if (w < x1 && w < y1 && w < z)
+        else if (w <= x1 && w <= y1 && w <= z)
         {
             Console.WriteLine("The lowest numver is " + w);
-        } else
-        {
-            Console.WriteLine("All numbers is Equal");
-        }
+        } 
     }
 
     //შევქმნათ მეთოდი, რომელმაც უნდა მიიღოს მეორე მეთოდის მეშვეობით მომხმარებლისგან ხილის დასახელება.
